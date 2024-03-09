09362d227f6ea71e5b3e03b9510a2906e8b36d2c
github <13472201613@163.com>
2024-03-09 23:22:23 +0800

    feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
